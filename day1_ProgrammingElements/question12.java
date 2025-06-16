ed08c27670bb63de8fe6f1e38a907d562acb6f94
anurag <anuraggupta8630@gmail.com>
2025-06-16 14:33:43 +0530

    Day1_programming

---
+++ day1_ProgrammingElements/question12.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class question12 {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       double perimeter= sc.nextDouble();
+        double length = perimeter/4;
+        System.out.println(" The length of the side is "+ length+"_ whose perimeter is "+perimeter);
+
+
+    }
+    
+}
